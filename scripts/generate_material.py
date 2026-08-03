1b81c6820a61d9b3209f9b8500500fff7b7eb22e
Paaswn <patsawin12@gmail.com>
2026-08-04 02:50:46 +0700

    prepare a compilation time color gen

--- scripts/generate_material.py
+++ scripts/generate_material.py
@@ -1,18 +1,22 @@
 import json
 
-OUTPUT = "/home/paaswn/Projects/Odin/projectF/src/simulation/generated_material.odin"
-CONFIG = "/home/paaswn/Projects/Odin/projectF/config/world_config.json"
+OUTPUT = "./src/simulation/generated_material.odin"
+CONFIG = "./config/world_config.json"
 
 with open(CONFIG, "r", encoding="utf-8") as f:
      materials = json.load(f)
 
-mats = "\t"
-for k in materials:
-    mats += k + ",\n\t"
-
+def make_all_material_name() -> str :
+    mats = "\t"
+    for k in materials:
+        mats += k + ",\n\t"
+    return mats
+    
 content = f"""package simulation
 
 // --- THIS FILE WAS AUTOMATICALLY GENERATED ---
+
+import rl "vendor:raylib"
 
 Material_Config :: struct {{
 	type:           Material_Type,
@@ -27,17 +31,40 @@ Material_Config :: struct {{
 	fall_drag:      f32,
 }}
 
-Material_Type :: enum u8 {{
-	Liquid, // move without thresh (side/slide thresh =0)
-	Powder, // move with thresh
-	Hard, // static material that can't be damaged by any game object
-	Semi_Hard, // static material that can be slightly damaged by game object
+Material :: enum u8 {{
+{make_all_material_name()}
 }}
 
-Material :: enum u8 {{
-{mats} 
+get_material_color :: proc(mat: Material, x, y: int, salt: u64) -> (color: rl.Color) {{
+	#partial switch mat {{
+	case .Empty:
+		color = rl.BLACK
+	case:
+		color = random_shade(get_material_base_color(mat), x, y, 20, salt)
+	case .Water:
+		color = rl.DARKBLUE
+    }}
+	return
 }}
+
+get_material_base_color :: proc(mat: Material) -> (color: rl.Color ) {{
+	switch mat {{
+	case .Empty:
+		color = rl.BLACK
+	case .Sand:
+		color = rl.BEIGE
+	case .Dirt:
+		color = rl.BROWN
+	case .Cement:
+		color = rl.DARKGRAY
+	case .Water:
+		color = rl.BLUE
+	}}
+	return
+}}
+
 """
 
 with open(OUTPUT, "w", encoding="utf-8") as f:
     f.write(content)
+
